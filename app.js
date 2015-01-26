acfba8047a135efb7729270d7f84817ae1edb328
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2015-01-26 17:39:46 +0900

    aaa

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,8 +24,13 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/edittest', function(req, res) {
+  var content = req.query.content;
+  io.emit('codechange', { content: content });
+  res.send(content);
+  res.end();
+});
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
@@ -55,15 +63,13 @@ app.use(function(err, req, res, next) {
     });
 });
 
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-server.listen(8000);
 
 io.on('connection', function(socket){
   socket.on('codechange', function(data) {
     console.log(data);
-    io.emit('codechange', data);
+    socket.broadcast.emit('codechange', data);
   });
 });
 
+server.listen(5000);
 module.exports = app;
